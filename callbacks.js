07dab95788d45bf5e699ce765604bc06e15d70be
Sushil <sushil_ghimire@homedepot.com>
2021-12-28 22:04:13 -0600

    promise try

---
+++ callbacks.js
@@ -0,0 +1,52 @@
+
+
+const posts = [{id:1,name:'ram'},{id:2,name:'anil'}];
+
+const createPosts = () => {
+
+    return new Promise((resolve,reject)=>{
+        setTimeout(function(){
+            posts.push({id:3,name:'shyam'});
+            const error = false;
+            if(!error){
+                resolve(posts);
+            }
+            else{
+                reject('sorry something went wrong');
+            }
+        
+           
+        },5000);
+
+       
+    });
+
+ 
+    
+
+};
+
+
+const getPosts = ()=>{
+    posts.forEach((x)=>{
+           console.log(x.name);
+
+    });
+}
+
+// createPosts().then(data=>{
+//     data.forEach((x)=>{
+//         console.log('name:',x.name);
+
+//  });
+// }).catch(error=>{
+//     console.log(error);
+// });
+
+async function init(){
+  const data =  await createPosts();
+    console.log('data=>',data);
+
+}
+
+init();
